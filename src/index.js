e4ca008252a1562797d51d1cdbb84ae32ae2c2c1
your_name <your_email>
2018-08-03 11:36:09 +0800

    add webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-const a = 1;
-let es5 = 'es5';
+import Rx from 'rxjs/Rx'
+
+const canvas = document.getElementsByTagName('canvas')[0];
+
+if (canvas.getContext){
+  var ctx = canvas.getContext('2d');
+  // drawing code here
+} else {
+  // canvas-unsupported code here
+}
 
 console.log('start learning!');
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports={
+    entry: './src/index.js',
+    devtool: 'eval-source-map',
+    output: {//'./dist/index.js',
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, 
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ['style-laoder', 'css-loader'],
+                include: path.join(__dirname, 'src'), //限制范围，提高打包速度
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"), //静态文件根目录
+        port: 9090, // 端口
+        host: 'localhost',
+        overlay: true,
+        compress: true // 服务器返回浏览器的时候是否启动gzip压缩
+    }
+}
